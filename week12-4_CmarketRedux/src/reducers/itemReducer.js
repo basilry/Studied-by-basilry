4d2ee2536556455c5751c2b20c5694b406fc69e0
basilry <basilry@gmail.com>
2021-05-23 23:16:53 +0900

    fileUpload

---
+++ week12-4_CmarketRedux/src/reducers/itemReducer.js
@@ -0,0 +1,34 @@
+import { REMOVE_FROM_CART, ADD_TO_CART, SET_QUANTITY } from "../actions/index";
+import { initialState } from "./initialState";
+
+const itemReducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case ADD_TO_CART: 
+    return Object.assign({}, state, {
+      cartItems: [...state.cartItems, action.payload]
+    })
+    
+    case REMOVE_FROM_CART:
+      let curItem = state.cartItems.filter(el => el.itemId !== action.payload.itemId)
+      return Object.assign({}, state, {
+        cartItems: curItem
+      })
+
+    case SET_QUANTITY:
+      let idx = state.cartItems.findIndex(el => el.itemId === action.payload.itemId)
+      return {
+        ...state,
+        cartItems: [
+          ...state.cartItems.slice(0, idx),
+          action.payload,
+          ...state.cartItems.slice(idx + 1),
+        ]
+      }
+      
+    default:
+      return state;
+  }
+}
+
+export default itemReducer;
